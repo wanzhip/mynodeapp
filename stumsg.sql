13cc9b338f1656f805aa49604a58477837f37224
万志鹏 <1151333166@qq.com>
2021-01-08 09:59:51 +0800

    sql文件

--- stumsg.sql
+++ stumsg.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50732
  File Encoding         : 65001
 
- Date: 08/01/2021 09:57:28
+ Date: 08/01/2021 09:59:29
 */
 
 SET NAMES utf8mb4;
@@ -31,16 +31,5 @@ CREATE TABLE `stumsg`  (
   `age` int(3) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of stumsg
--- ----------------------------
-INSERT INTO `stumsg` VALUES (1, '唐三', '1', '武魂', '1', '史莱克学院', 20);
-INSERT INTO `stumsg` VALUES (2, '小舞', '2', '武魂', '1', '史莱克学院', 18);
-INSERT INTO `stumsg` VALUES (3, '三炮', '3', '修仙', '2', '史莱克学院', 8);
-INSERT INTO `stumsg` VALUES (4, '宁荣荣', '4', '武魂', '1', '史莱克学院', 18);
-INSERT INTO `stumsg` VALUES (7, '小刚', '123', '武魂', '1', '史莱克学院', 20);
-INSERT INTO `stumsg` VALUES (8, '宁风致', '123', '武魂', '12', '史莱克学院', 20);
-INSERT INTO `stumsg` VALUES (10, '戴沐白', '2020001', '武魂', '1', '史莱克学院', 20);
 
 SET FOREIGN_KEY_CHECKS = 1;
